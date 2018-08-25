9dc5138c13b9dfd064327ac41f9d7adbb7bdad14
paulo1983 <paulo-animacion@cardozo-animacion.com.ar>
2018-08-25 00:36:09 -0300

    primer commit

---
+++ Proyectos/Proyecto1/cmake-build-debug/CMakeFiles/Proyecto1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Algo1/Proyectos/Proyecto1/labo00.cpp" "E:/Algo1/Proyectos/Proyecto1/cmake-build-debug/CMakeFiles/Proyecto1.dir/labo00.cpp.obj"
+  "E:/Algo1/Proyectos/Proyecto1/main.cpp" "E:/Algo1/Proyectos/Proyecto1/cmake-build-debug/CMakeFiles/Proyecto1.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Proyectos/Proyecto1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+/*
+int main(){
+    string frase;
+    ifstream fi("texto.txt");
+
+      while (getline(fi,frase))
+        {
+            std::cout << frase << "\n";
+        }
+        fi.close();
+
+    return 0;
+}
+ */
